6585f03a0a8c5b8ac5c8ea9f1c4a0bce1eb5e740
Rafael Polonio <rafael.polonio@gmail.com>
2023-08-02 20:21:36 -0300

    professor cards

--- src/components/Cards/ProfessorCards.jsx
+++ src/components/Cards/ProfessorCards.jsx
@@ -17,50 +17,27 @@ const ProfessorCard = ({ title, description, imageUrl, videoUrl }) => {
 
   console.log(modalOpen)
   return (
-  <WhiteCard sx={{ maxWidth: 345, borderRadius: '11px' }}>
-    <CardMedia
-      component="img"
-      alt={title}
-      height="240"
-      image={imageUrl}
-    />
-    <CardContent>
-      <Typography gutterBottom variant="h5" component="div" sx={{ color: '#00000', fontWeight: 'bold' }}>
-        {title}
-      </Typography>
-      <Box style={{ height: '130px', overflow: 'hidden' }}>
-        <Typography variant="body2" color="text.secondary" sx={{ color: '#00000', fontWeight: 'bold' }}>
-          {description}
-        </Typography>
-      </Box>
-    </CardContent>
-    <CardActions>
-      <Button variant="contained" size="small" onClick={() => handleModalOpen(videoUrl)} >Learn More</Button>
-    </CardActions>
-
-    {/* <Modal open={modalOpen} onClose={handleModalClose}>
-        <Box
-            sx={{
-              position: 'absolute',
-              top: '50%',
-              left: '50%',
-              transform: 'translate(-50%, -50%)',
-              bgcolor: 'background.paper',
-              outline: 'none',
-            }}
-          >
-            <iframe
-              width="660"
-              height="415"
-              src={selectedVideo + "?autoplay=1&mute=0"} // Adding autoplay and mute options
-              title="YouTube video player"
-              frameBorder="0"
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-              allowFullScreen
-            ></iframe>
+    <WhiteCard sx={{ maxWidth: 345, borderRadius: '11px' }}>
+        <CardMedia
+        component="img"
+        alt={title}
+        height="240"
+        image={imageUrl}
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div" sx={{ color: '#00000', fontWeight: 'bold' }}>
+              {title}
+          </Typography>
+          <Box style={{ height: '130px', overflow: 'hidden' }}>
+              <Typography variant="body2" color="text.secondary" sx={{ color: '#00000', fontWeight: 'bold' }}>
+              {description}
+              </Typography>
           </Box>
-      </Modal> */}
-  </WhiteCard>
+        </CardContent>
+        <CardActions>
+          <Button variant="contained" size="small" onClick={() => handleModalOpen(videoUrl)} >Learn More</Button>
+        </CardActions>
+    </WhiteCard>
   )
 }
 
